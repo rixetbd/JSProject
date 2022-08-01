bcee70e23dd775fc92a64e8dbde02a6edea15985
Muhammad Rabiul <rixetbd@gmail.com>
2022-08-01 18:43:38 +0600

    1s

--- multiple_filter/js/app.js
+++ multiple_filter/js/app.js
@@ -12,12 +12,16 @@ let industry_Name_ID = document.getElementById('industry_Name_ID');
 for (let i = 0; i < countryNames.length; i++) {
     let boxDiv = document.createElement("div");
     let country_box = document.createElement("div");
+    let country_Name = document.createElement("div");
     boxDiv.className = "col-md-3";
     country_box.className = "country_box";
-    country_box.setAttribute('id', countryNames[i]);
+    country_Name.className = "industry_box_overly";
+    country_Name.setAttribute('id', countryNames[i]);
+    country_box.style.backgroundImage = `url('img/Country_${countryNames[i]}.jpg')`;
     country_Name_ID.appendChild(boxDiv);
     boxDiv.appendChild(country_box);
-    country_box.textContent = countryNames[i];
+    country_box.appendChild(country_Name);
+    country_Name.textContent = countryNames[i];
 }
 
 // for (let i = 0; i < stateNames.length; i++) {
@@ -34,12 +38,16 @@ for (let i = 0; i < countryNames.length; i++) {
 for (let i = 0; i < industryNames.length; i++) {
     let boxDiv3 = document.createElement("div");
     let industry_box = document.createElement("div");
+    let industry_Name = document.createElement("div");
     boxDiv3.className = "col-md-2";
     industry_box.className = "industry_box";
-    industry_box.setAttribute('id', industryNames[i]);
+    industry_Name.className = "industry_box_overly";
+    industry_Name.setAttribute('id', industryNames[i]);
+    industry_box.style.backgroundImage = `url('img/${industryNames[i]}.jpg')`;
     industry_Name_ID.appendChild(boxDiv3);
     boxDiv3.appendChild(industry_box);
-    industry_box.textContent = industryNames[i];
+    industry_box.appendChild(industry_Name);
+    industry_Name.textContent = industryNames[i];
 }
 
 $("#country_Name_ID div").click(function (event) {
@@ -48,10 +56,16 @@ $("#country_Name_ID div").click(function (event) {
     $('#industry').removeClass('d-none');
 });
 
-$("#industry_Name_ID div").click(function (event) {
+$("#industry_Name_ID div div div").click(function (event) {
     let industry = event.target.id;
     localStorage.setItem("industry", industry);
     $('#submit').removeClass('disabled');
+
+    // let AllClass = document.querySelectorAll('industry_box_overly');
+    // AllClass.style.backgroundColor = "rgba(0,0,0,0.5)";
+
+    let idAct = document.getElementById(industry); 
+    idAct.style.backgroundColor = "rgb(0 0 0 / 11%)";
 });
 
 $('#submit').click(function(){
