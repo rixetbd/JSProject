63d992550c0dcc967604b0fb9dd3738ede2f9eb2
Muhammad Rabiul <rixetbd@gmail.com>
2022-08-13 16:31:51 +0600

    1s

--- multiple_filter2/js/table-filter.js
+++ multiple_filter2/js/table-filter.js
@@ -3,8 +3,9 @@ let table_Email = document.getElementById('table_Email');
 let table_Phone = document.getElementById('table_Phone');
 let table_Company = document.getElementById('table_Company');
 let table_Revenue = document.getElementById('table_Revenue');
-let table_City = document.getElementById('table_Name');
+let table_City = document.getElementById('table_City');
 let table_URL = document.getElementById('table_URL');
+let table_refresh = document.getElementById('table_refresh');
 
 function show_hide_column() {
     var tbl = document.getElementById('myTable');
@@ -12,21 +13,28 @@ function show_hide_column() {
 
     for (var row = 0; row < rows.length; row++) {
         var cols = rows[row].children;
-        console.log(1, cols.length);
+        // console.log(1, cols.length);
         if (4 >= 0 && 4 < cols.length) {
             var cell = cols[4];
-            console.log(cell, cell.tagName);
+            // console.log(cell, cell.tagName);
             if (cell.tagName == 'td') cell.style.display = 'none';
         }
     }
 }
 
-table_Name.addEventListener('click', function () {
+function signShowHide(sign){
+    if(sign.className == 'fas fa-plus'){
+        sign.setAttribute('class', "fas fa-minus");
+    }else{
+        sign.setAttribute('class', "fas fa-plus");
+    }
+}
 
-    let sign = document.getElementsByTagName('i');
 
-};
 table_Name.addEventListener('click', function () {
+    var sign = table_Name.querySelector('i');
+    signShowHide(sign);
+
     show_hide_column();
     var lastColHeader = Array.prototype.slice.call(document.querySelectorAll('th:first-child', 'table'), 0); // get the header cell
     var lastColCells = Array.prototype.slice.call(document.querySelectorAll('td:first-child', 'table'), 0).concat(lastColHeader); // get the column cells, and add header
@@ -36,6 +44,9 @@ table_Name.addEventListener('click', function () {
 });
 
 table_Email.addEventListener('click', function () {
+    var sign = table_Email.querySelector('i');
+    signShowHide(sign);
+
     show_hide_column();
     var lastColHeader = Array.prototype.slice.call(document.querySelectorAll('th:nth-child(2)', 'table'), 0); // get the header cell
     var lastColCells = Array.prototype.slice.call(document.querySelectorAll('td:nth-child(2)', 'table'), 0).concat(lastColHeader); // get the column cells, and add header
@@ -45,6 +56,9 @@ table_Email.addEventListener('click', function () {
 });
 
 table_Phone.addEventListener('click', function () {
+    var sign = table_Phone.querySelector('i');
+    signShowHide(sign);
+
     show_hide_column();
     var lastColHeader = Array.prototype.slice.call(document.querySelectorAll('th:nth-child(3)', 'table'), 0); // get the header cell
     var lastColCells = Array.prototype.slice.call(document.querySelectorAll('td:nth-child(3)', 'table'), 0).concat(lastColHeader); // get the column cells, and add header
@@ -53,6 +67,9 @@ table_Phone.addEventListener('click', function () {
     });
 });
 table_Company.addEventListener('click', function () {
+    var sign = table_Company.querySelector('i');
+    signShowHide(sign);
+
     show_hide_column();
     var lastColHeader = Array.prototype.slice.call(document.querySelectorAll('th:nth-child(4)', 'table'), 0); // get the header cell
     var lastColCells = Array.prototype.slice.call(document.querySelectorAll('td:nth-child(4)', 'table'), 0).concat(lastColHeader); // get the column cells, and add header
@@ -61,6 +78,9 @@ table_Company.addEventListener('click', function () {
     });
 });
 table_Revenue.addEventListener('click', function () {
+    var sign = table_Revenue.querySelector('i');
+    signShowHide(sign);
+
     show_hide_column();
     var lastColHeader = Array.prototype.slice.call(document.querySelectorAll('th:nth-child(5)', 'table'), 0); // get the header cell
     var lastColCells = Array.prototype.slice.call(document.querySelectorAll('td:nth-child(5)', 'table'), 0).concat(lastColHeader); // get the column cells, and add header
@@ -69,6 +89,9 @@ table_Revenue.addEventListener('click', function () {
     });
 });
 table_City.addEventListener('click', function () {
+    var sign = table_City.querySelector('i');
+    signShowHide(sign);
+
     show_hide_column();
     var lastColHeader = Array.prototype.slice.call(document.querySelectorAll('th:nth-child(6)', 'table'), 0); // get the header cell
     var lastColCells = Array.prototype.slice.call(document.querySelectorAll('td:nth-child(6)', 'table'), 0).concat(lastColHeader); // get the column cells, and add header
@@ -77,10 +100,21 @@ table_City.addEventListener('click', function () {
     });
 });
 table_URL.addEventListener('click', function () {
+    var sign = table_URL.querySelector('i');
+    signShowHide(sign);
+
     show_hide_column();
     var lastColHeader = Array.prototype.slice.call(document.querySelectorAll('th:nth-child(7)', 'table'), 0); // get the header cell
     var lastColCells = Array.prototype.slice.call(document.querySelectorAll('td:nth-child(7)', 'table'), 0).concat(lastColHeader); // get the column cells, and add header
     lastColCells.forEach(function (cell) { // iterate and hide
         cell.style.display = 'none';
     });
+});
+
+table_refresh.addEventListener('click', ()=>{
+
+    table_Name.setAttribute('class', "fas fa-plus");
+    table_Name.setAttribute('class', "fas fa-plus");
+    table_Name.setAttribute('class', "fas fa-plus");
+
 });
